4fe49207a4143ccdc05d6261e88fdc784d5d3b6f
Yari G <jebbygonzalez@gmail.com>
2025-09-04 21:01:34 +0000

    Bmi calulations print out fine.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int main() {
- 
+const int height = 61; 
+const int weight= 130;
 
+double kg;
+double meter;
 
+//calulations
+kg = weight * 0.453592;
+meter = height * 0.0254;
+
+//bmi forumla
+double bmi;
+bmi = kg/(meter * meter);
+
+cout << "The BMI for this person is : " << bmi << endl;
 }
